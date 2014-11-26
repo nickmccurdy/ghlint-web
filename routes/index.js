1a64b16ca2929fcd620c0db2619d7fea58b4551b
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-26 16:15:02 -0500

    Add basic ghlint integration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var ghlint = require('ghlint');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  ghlint.lintAll().then(function (results) {
+    res.render('index', {
+      title: 'Express',
+      results: results
+    });
+  });
 });
 
 module.exports = router;
